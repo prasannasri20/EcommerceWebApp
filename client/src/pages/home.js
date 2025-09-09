4d4f5a85a76c32a79f188981585f64ac1bcda5fd
yodhathecoder <yodhakranthi@gmail.com>
2025-09-09 22:29:40 +0530

    just add signout button

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -65,6 +65,14 @@ export default function Products() {
   Cart ({cartCount})
 </button>
 
+         <button
+  onClick={() => navigate('/login')}
+  className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+>
+  SignOut
+</button>
+
+
 
       </div>
 
